b5260ef0b70551b3235638633e51fb8f1de1b53d
Smily Os <osama2231@hotmail.com>
2025-03-08 01:39:42 -0500

    Add a search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,24 @@
 import { useState } from 'react'
+import searchIcon from '../../assets/icons8-search.svg'
+import styles from './SearchBar.module.css'
 
 export default function SearchBar() {
+    const [search, setSearch] = useState('');
 
+    const handleInputChange = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const searchForSong = () => {
+        alert('Searching for song: ' + search);
+    }
+
+    return(
+        <>
+            <div className={styles.container}>
+                <input name="query" placeholder="Search by name" value={search} onChange={handleInputChange} className={styles.searchBar} />
+                <img src={searchIcon} alt="Search icon" onClick={searchForSong} className={styles.searchIcon} />
+            </div>
+        </>
+    )
 }
